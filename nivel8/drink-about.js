5e8dacbf6ded41a24af20af97cebaa26089bf1b0
Julio <juliofornode@gmail.com>
2017-08-21 14:05:41 -0700

    julio 1

---
+++ nivel8/drink-about.js
@@ -0,0 +1,18 @@
+
+/*
+https://www.codewars.com/kata/drink-about
+*/
+
+//julio
+
+function peopleWithAgeDrink(age) {
+  if ( age < 14 ) {
+    return "drink toddy";
+  } else if ( age < 18 ) {
+    return "drink coke";
+  } else if ( age < 21 ) {
+    return "drink beer";
+  } else {
+    return "drink whisky";
+  }
+}
