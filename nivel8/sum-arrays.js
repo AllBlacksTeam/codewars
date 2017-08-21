9421cc57baf074904d5ab216021e68e01b332643
Julio <juliofornode@gmail.com>
2017-08-21 14:21:10 -0700

    julio, sum arrays

---
+++ nivel8/sum-arrays.js
@@ -0,0 +1,12 @@
+
+// http://www.codewars.com/kata/sum-arrays
+
+// julio
+
+function sum(myArray) {
+  var total = 0;
+  for(var i=0; i<myArray.length; i++) {
+    total = total + myArray[i];
+  }
+  return total;
+}
