069958c99629a86805fdedc9783cd3126efa645e
David F.E <david@MacBook-Air-de-David.local>
2017-08-21 14:37:05 +0200

    David Smash

--- nivel8/sentence-smash.js
+++ nivel8/sentence-smash.js
@@ -1,8 +1,14 @@
-
 // http://www.codewars.com/kata/sentence-smash
 
 // julio
 
 function smash(myArray) {
   return myArray.join(' ');
+}
+
+// david
+
+function smash(words) {
+  'use strict';
+  return words.join(' ');
 }
