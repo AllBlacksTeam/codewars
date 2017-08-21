b05f471ce1746ce8b87096d40f6042b572a3d460
David F.E <david@MacBook-Air-de-David.local>
2017-08-21 15:24:55 +0200

    David Sum arrays

--- nivel8/sum-arrays.js
+++ nivel8/sum-arrays.js
@@ -1,12 +1,24 @@
-
 // http://www.codewars.com/kata/sum-arrays
 
 // julio
 
 function sum(myArray) {
   var total = 0;
-  for(var i=0; i<myArray.length; i++) {
+  for (var i = 0; i < myArray.length; i++) {
     total = total + myArray[i];
   }
+  return total;
+}
+
+// david
+
+function sum(numbers) {
+  'use strict';
+
+  var total = 0;
+  for (var i in numbers) {
+    total += numbers[i];
+  }
+
   return total;
 }
