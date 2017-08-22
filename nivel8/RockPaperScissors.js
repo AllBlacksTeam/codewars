9ed4ee513129c187674eb831337ef9fb8e559a1f
Julio <juliofornode@gmail.com>
2017-08-21 22:52:23 -0700

    julio rock paper scissors

--- nivel8/RockPaperScissors.js
+++ nivel8/RockPaperScissors.js
@@ -15,3 +15,59 @@ const rps = (p1, p2) => {
 
   return winner;
 };
+
+/*
+
+COMENTARIO: se recomienda utilizar === en vez de ==
+
+*/
+
+
+
+// julio
+
+function rps(player1, player2) {
+  var one = 'Player 1 won!';
+  var two = 'Player 2 won!';
+  var draw = 'Draw!';
+
+  if (player1 === 'scissors') {
+    switch(player2) {
+    case 'paper':
+        return one;
+        break;
+    case 'rock':
+        return two;
+        break;
+    default:
+        return draw;
+    }
+  }
+
+  else if (player1 === 'paper') {
+    switch(player2) {
+    case 'rock':
+        return one;
+        break;
+    case 'scissors':
+        return two;
+        break;
+    default:
+        return draw;
+    }
+  }
+
+  else if (player1 === 'rock') {
+    switch(player2) {
+    case 'scissors':
+        return one;
+        break;
+    case 'paper':
+        return two;
+        break;
+    default:
+        return draw;
+    }
+  }
+
+}
