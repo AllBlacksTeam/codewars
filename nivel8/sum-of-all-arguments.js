0344dec76a1e60372e3fbcf8bb2352690431502a
Julio <juliofornode@gmail.com>
2017-08-22 00:00:27 -0700

    julio sum-of-all-arguments

---
+++ nivel8/sum-of-all-arguments.js
@@ -0,0 +1,16 @@
+// http://www.codewars.com/kata/sum-of-all-arguments
+
+// julio
+
+function sum() {
+  var total=0;
+  for (var i = 0; i < arguments.length; i++)
+      total = total + arguments[i];
+  return total;
+}
+
+/*
+
+COMENTARIO: arguments es el array de argumentos por defecto en javascript
+
+*/
