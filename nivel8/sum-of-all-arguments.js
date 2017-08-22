f394647505d94edb018f218d21f5b8accd4a543b
David F.E <david@MacBook-Air-de-David.local>
2017-08-22 11:02:37 +0200

    David Sum of all Arguments

--- nivel8/sum-of-all-arguments.js
+++ nivel8/sum-of-all-arguments.js
@@ -3,9 +3,8 @@
 // julio
 
 function sum() {
-  var total=0;
-  for (var i = 0; i < arguments.length; i++)
-      total = total + arguments[i];
+  var total = 0;
+  for (var i = 0; i < arguments.length; i++) total = total + arguments[i];
   return total;
 }
 
@@ -14,3 +13,10 @@ function sum() {
 COMENTARIO: arguments es el array de argumentos por defecto en javascript
 
 */
+
+// david
+function sum() {
+  var total = 0;
+  for (var i in arguments) total += arguments[i];
+  return total;
+}
