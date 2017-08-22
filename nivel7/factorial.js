ac6eccec21caff1ead897ec76181f7ad0b048a5b
David F.E <david@MacBook-Air-de-David.local>
2017-08-22 13:37:05 +0200

    David factorial

--- nivel7/factorial.js
+++ nivel7/factorial.js
@@ -5,8 +5,10 @@
 function factorial(n) {
   if ((n < 0) | (n > 12))
     throw new RangeError('Parameter must be between 0 and 12');
-
+  /*
   var f = 1;
   for (i = 1; i <= n; i++) f = f * i;
-  return f; // `factorial for ${n} is ${f}`;
+*/
+  if (n === 0) return 1;
+  return n * factorial(n - 1); // `factorial for ${n} is ${f}`;
 }
